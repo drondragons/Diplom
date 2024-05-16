0cf0bb3add2efa4b3fb4d22e7e2a2eb4a9fa5a59
drondragons <66682810+drondragons@users.noreply.github.com>
2024-05-17 02:43:51 +0300

    Добавил класс meter в пакет measurement

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src import Validator, NumberValidator, FractionValidator, IntValidator, Dec
 
 from fractions import Fraction
 from decimal import Decimal
+import math
+import sys
 
 from src.measurement import Meter
 
@@ -15,9 +17,42 @@ def main() -> None:
     
     print("Meter")
     for i in range(20):
-        print(Meter(i))
+        print(Meter(Fraction(i)))
     meter = Meter(2)
     print(meter)
+    
+    t = [dir(int), dir(float), dir(Decimal), dir(Fraction)]
+    for item in t:
+        print("__sizeof__" in item)
+        
+    print(math.floor(Meter(Fraction(12323/234))))
+    print(math.ceil(Meter(Fraction(12323/234))))
+    print(math.trunc(Meter(Fraction(12323/234))))
+    print(Meter(Fraction(12323/234)).__int__())
+    print(Meter(Fraction(12323/234)).__float__())
+    print(int(1).__float__())
+    print(float(1.12434923).__float__())
+    print(int(1).__sizeof__())
+    print(float(1.23).__sizeof__())
+    print(sys.getsizeof(1))
+    print(sys.getsizeof(1.9245))
+    print(sys.getsizeof(Meter(Fraction(123/243))))
+    print(Meter(Fraction(123/243)).__sizeof__())
+    print(sys.getsizeof(object()))
+    
+    
+    # '__add__', '__and__', '__bool__', '__class__',
+    # '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__',
+    # '__floordiv__', '__ge__', '__getattribute__',
+    # '__getnewargs__', '__gt__', '__init_subclass__',
+    # '__le__', '__lshift__', '__lt__', '__mod__', '__mul__',
+    # '__ne__', '__new__', '__or__', '__pow__', '__radd__', '__rand__',
+    # '__rdivmod__', '__reduce__', '__reduce_ex__', '__rfloordiv__',
+    # '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__rpow__', 
+    # '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', 
+    # '__sizeof__', '__sub__', '__subclasshook__', '__truediv__', 
+    # '__xor__', 'as_integer_ratio', 'bit_count', 'bit_length', 'conjugate', 'denominator',
+    # 'from_bytes', 'imag', 'numerator', 'real', 'to_bytes'
     
 
 if __name__ == "__main__":
--- src/measurement/length.py
+++ src/measurement/length.py
@@ -1,3 +1,5 @@
+import sys
+import math
 from ..validators.constants import NUMBER_TYPES
 from .. import format_number, format_plural_form
 from ..validators.number_validator import NumberValidator
@@ -9,20 +11,24 @@ __all__ = [
 
 class Meter:
     
+    __slots__ = [
+        "__value",
+    ]
+    
     SHORT_FORM = "м"
     FULL_FORM = "метр"
     PREFIX_METER = str()
     
     @property
     def value(self) -> NUMBER_TYPES:
-        return self._value
+        return self.__value
     
     @value.setter
     def value(self, value: object) -> None:
         exception, message = NumberValidator.validate(value, NUMBER_TYPES, 0)
         if exception:
             raise exception(f"'{self.__class__.__name__}': " + message)
-        self._value = value
+        self.__value = value
         
     def __init__(self, value: object = DEFAULT_LENGTH_VALUE) -> None:
         self.value = value
@@ -33,6 +39,52 @@ class Meter:
         
     def __str__(self) -> str:
         return f"{self.__format_value()}\n"
+    
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__} (value: {self.value})"
+    
+    def __hash__(self) -> int:
+        return hash(self.value)
+    
+    def __format__(self, format_spec: str = str(), /) -> "Meter":
+        raise TypeError(f"'{self.__class__.__name__}': Операция форматирования (format(self)) недоступна!")
+    
+    def __index__(self) -> "Meter":
+        raise TypeError(f"'{self.__class__.__name__}': Операция индексирования (self[index]) недоступна!")
+    
+    # ------------------- Unary operators ---------------------------
+    
+    def __pos__(self) -> "Meter":
+        return self
+    def __neg__(self) -> "Meter":
+        raise TypeError(f"'{self.__class__.__name__}': Бинарная операция отрицания (-self) недоступна!")
+    
+    def __abs__(self) -> "Meter":
+        return self
+    
+    def __invert__(self) -> "Meter":
+        raise TypeError(f"'{self.__class__.__name__}': Бинарная операция инверсии (~self) недоступна!")
+    
+    def __floor__(self) -> "Meter":
+        return Meter(math.floor(self.value))
+    def __ceil__(self) -> "Meter":
+        return Meter(math.ceil(self.value))
+    def __trunc__(self) -> "Meter":
+        return Meter(math.trunc(self.value))
+    
+    def __int__(self) -> int:
+        return math.floor(self.value)
+    def __float__(self) -> float:
+        return float(self.value)
+    
+    # def __sizeof__(self) -> int:
+    #     return 
+        # return sys.getsizeof(object()) + sys.getsizeof(self.value)
+    
+    # ------------------- Binary operators ---------------------------
+    
+    def __round__(self, n: int = 0) -> "Meter":
+        return Meter(round(self.value, n))
     
     
 class KiloMeter(Meter):
