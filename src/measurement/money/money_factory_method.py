b432797a22a77c9a30e2c28d59b7afaa31ee700a
drondragons <66682810+drondragons@users.noreply.github.com>
2024-05-18 22:03:28 +0300

    Добавил функционал в пакет money

--- src/measurement/money/money_factory_method.py
+++ src/measurement/money/money_factory_method.py
@@ -1,4 +1,4 @@
-from .money import Money
+from .money import *
 from .money import MONEY_CLASSES
 
 from .. import REAL_TYPES
@@ -10,6 +10,10 @@ from ...validators import Validator
 
 __all__ = [
     "MoneyFactoryMethod",
+    "RubleFactoryMethod",
+    "DollarFactoryMethod",
+    "EuroFactoryMethod",
+    "YuanFactoryMethod",
 ]
 
 
@@ -23,13 +27,61 @@ class MoneyFactoryMethod(RealFactoryMethod):
         minimum: Money | REAL_TYPES = DEFAULT_MINIMUM_VALUE, 
         maximum: Money | REAL_TYPES = RealFactoryMethod.DEFAULT_MAXIMUM_VALUE,
         is_int: bool = True,
-        meter_type: type = Money
+        money_type: type = Money
     ) -> Money:
         minimum = Money(minimum)
         maximum = Money(maximum)
         
-        if meter_type not in MONEY_CLASSES:
-            message = f"Недопустимый тип '{meter_type.__name__}'! Ожидался тип {Validator.format_union_types(meter_type)}!"
+        if money_type not in MONEY_CLASSES:
+            message = f"Недопустимый тип '{money_type.__name__}'! Ожидался тип {Validator.format_union_types(money_type)}!"
             raise TypeError(f"\n\t{cls.__name__}.convert: " + message)
         
-        return meter_type(super().generate(minimum.value, maximum.value, is_int))
+        return money_type(super().generate(minimum.value, maximum.value, is_int))
+    
+    
+class RubleFactorMethod(MoneyFactoryMethod):
+    
+    @classmethod
+    def generate(
+        cls,
+        minimum: Money | REAL_TYPES = MoneyFactoryMethod.DEFAULT_MINIMUM_VALUE, 
+        maximum: Money | REAL_TYPES = RealFactoryMethod.DEFAULT_MAXIMUM_VALUE,
+        is_int: bool = True
+    ) -> Ruble:
+        return super().generate(minimum, maximum, is_int, Ruble)
+    
+    
+class DollarFactorMethod(MoneyFactoryMethod):
+    
+    @classmethod
+    def generate(
+        cls,
+        minimum: Money | REAL_TYPES = MoneyFactoryMethod.DEFAULT_MINIMUM_VALUE, 
+        maximum: Money | REAL_TYPES = RealFactoryMethod.DEFAULT_MAXIMUM_VALUE,
+        is_int: bool = True
+    ) -> Dollar:
+        return super().generate(minimum, maximum, is_int, Dollar)
+    
+    
+class EuroFactorMethod(MoneyFactoryMethod):
+    
+    @classmethod
+    def generate(
+        cls,
+        minimum: Money | REAL_TYPES = MoneyFactoryMethod.DEFAULT_MINIMUM_VALUE, 
+        maximum: Money | REAL_TYPES = RealFactoryMethod.DEFAULT_MAXIMUM_VALUE,
+        is_int: bool = True
+    ) -> Euro:
+        return super().generate(minimum, maximum, is_int, Euro)
+    
+    
+class YuanFactorMethod(MoneyFactoryMethod):
+    
+    @classmethod
+    def generate(
+        cls,
+        minimum: Money | REAL_TYPES = MoneyFactoryMethod.DEFAULT_MINIMUM_VALUE, 
+        maximum: Money | REAL_TYPES = RealFactoryMethod.DEFAULT_MAXIMUM_VALUE,
+        is_int: bool = True
+    ) -> Yuan:
+        return super().generate(minimum, maximum, is_int, Yuan)
